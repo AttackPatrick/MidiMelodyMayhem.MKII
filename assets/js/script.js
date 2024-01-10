769438a3d42fed4ca8841281c79cdbb409e9b242
AttackPatrick <>
2024-01-10 17:15:32 +0000

    Add pause button to the question box and create an image folder to contain the the pause image

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const startButton = document.getElementById('start-button');
+const welcomeContainer = document.getElementById('welcome-container');
+
+
+startButton.addEventListener('click', (function() {
+
+    welcomeContainer.style.display = 'none';
+  }))
